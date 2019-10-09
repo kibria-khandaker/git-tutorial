feb562ff6924eb54fb0d73e1446a6d7b4b7d30c6
kibria khandaker <kibriakhandakera66@gmail.com>
2019-10-09 17:46:31 +0600

    added a new method called displa

--- Hello.java
+++ Hello.java
@@ -2,4 +2,7 @@ public class Hello{
 	public static void main(String[] args) {
 		System.out.println("Sublime Text is Awesome...");
 	}
+	public static void greeting(String[] args) {
+		System.out.println("Welcome" + name);
+	}
 }
