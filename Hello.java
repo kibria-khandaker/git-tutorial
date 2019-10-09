cf0e645ff176a6f8be55c3ab4aa3b3200173d5bc
kibria khandaker <kibriakhandakera66@gmail.com>
2019-10-09 17:52:21 +0600

    added a new method called display

--- Hello.java
+++ Hello.java
@@ -1,8 +1,10 @@
 public class Hello{
 	public static void main(String[] args) {
 		System.out.println("Sublime Text is Awesome...");
+		
+		greeting("kibria khandaker")
 	}
-	public static void greeting(String[] args) {
+	public static void greeting(String name) {
 		System.out.println("Welcome" + name);
 	}
 }
